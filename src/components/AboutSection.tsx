c4d29dc4a3851dfdab593cf9a16682bcf57f940d
aqulesz <franmoyano123@gmail.com>
2025-05-23 19:31:00 -0300

    base de la seccion hecha en mobile

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,10 +1,12 @@
 function AboutSection() {
     return (
-        <div className="bg-black w-full h-dvh flex justify-center py-15">
-            <div className="w-2/3">
-                <h2 className="text-white font-default font-bold text-5xl mb-10">¿Qué es <span className="font-logo "> la cocina ?</span></h2>
-                <p className="text-red-900 text-4xl font-default font-bold italic">Un espacio donde la música, la danza, el teatro y las charlas con grandes artistas se unen para que vivas el arte desde adentro.</p>
-            </div>
+        <div className="bg-[url('/src/assets/grades-656593_1920.webp')] bg-black/80 bg-blend-overlay bg-cover bg-center w-full flex flex-col p-6">
+                <h3 className="text-xs px-2 text-left text-custom-red font-default font-bold" >Sobre nosotros</h3>
+                <h2 className="text-white font-default font-bold text-3xl text-center mb-5">¿Qué es <span className="font-logo text-gray-300"> la cocina</span> ?</h2>
+                <p className="text-white text-lg font-default font-bold italic text-center mb-5">Un espacio donde la música, la danza, el teatro y las charlas con grandes artistas se unen para que vivas el arte desde adentro.</p>
+                <img src="/src/assets/guitar-5187376_1280.webp" alt="" className="rounded-2xl saturate-0 mb-5"/>
+                <h2 className="text-white font-default font-bold text-3xl text-center mb-5">¿Quienes somos?</h2>
+                <p className="text-white text-lg font-default font-bold italic text-center mb-5">Un espacio donde la música, la danza, el teatro y las charlas con grandes artistas se unen para que vivas el arte desde adentro.</p>
         </div>
     )
 }
