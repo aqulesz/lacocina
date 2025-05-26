86eca1bb8b54166e51804f5b259424b98fefdbd8
aqulesz <franmoyano123@gmail.com>
2025-05-26 17:38:44 -0300

    ahora si esta fixeado el tema del scroll en el hero

--- src/components/PrincipalHero.tsx
+++ src/components/PrincipalHero.tsx
@@ -1,6 +1,6 @@
 function PrincipalHero() {
     return (
-        <div className="bg-[url('/src/assets/concert-7424190_1920.webp')] w-full h-screen flex justify-center items-center bg-cover bg-center">
+        <div className="bg-[url('/src/assets/concert-7424190_1920.webp')] w-full h-svh flex justify-center items-center bg-cover bg-center">
             < div className="absolute inset-0 bg-black opacity-25" ></div>
             <div className="z-1 flex flex-col gap-7 w-xs h-1/3 p-3 sm:w-2/3 lg:w-4/5 lg:justify-between lg:items-center xl:w-4/6">
                 <h1 className="text-5xl font-hero font-bold tracking-tight text-white sm:text-7xl md:text-7xl lg:text-8xl text-center lg:mb-5">
