6baa0581dfce299df74125a6f6d0f1560b28831b
aqulesz <franmoyano123@gmail.com>
2025-05-26 17:25:34 -0300

    probando algo

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,9 +1,11 @@
 function AboutSection() {
     return (
-        <div className="bg-black w-full h-dvh flex justify-center py-15">
+        <div className="bg-black w-full flex justify-center py-15">
             <div className="w-2/3">
                 <h2 className="text-white font-default font-bold text-5xl mb-10">¿Qué es <span className="font-logo "> la cocina ?</span></h2>
-                <p className="text-red-900 text-4xl font-default font-bold italic">Un espacio donde la música, la danza, el teatro y las charlas con grandes artistas se unen para que vivas el arte desde adentro.</p>
+                <p className="text-red-900 text-lg font-default font-bold italic">Un espacio donde la música, la danza, el teatro y las charlas con grandes artistas se unen para que vivas el arte desde adentro.</p>
+                <p className="text-red-900 text-lg font-default font-bold italic">Un espacio donde la música, la danza, el teatro y las charlas con grandes artistas se unen para que vivas el arte desde adentro.</p>
+                <p className="text-red-900 text-lg font-default font-bold italic">Un espacio donde la música, la danza, el teatro y las charlas con grandes artistas se unen para que vivas el arte desde adentro.</p>
             </div>
         </div>
     )
