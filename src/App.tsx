41eb2c47cf64db665ae6e7c000f08947e9aa3430
aqulesz <franmoyano123@gmail.com>
2025-06-04 23:43:20 -0300

    seo 3

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,11 @@ import PrincipalHero from './components/PrincipalHero'
 import AboutSection from './components/AboutSection'
 import ClasesSection from './components/ClasesSection'
 import ContactSection from './components/ContactSection'
-import { Helmet } from 'react-helmet';
 
 function App() {
 
   return (
     <>
-      <Helmet>
-        <title>La Cocina Formación y Producción Artística - Academia de musica en Córdoba</title>
-        <meta name="description" content="Academia de música en Córdoba. Clases de guitarra, piano, canto, folclore y producción musical en La Cocina Formación Artística." />
-        <meta name="author" content="La Cocina Formación y Producción Artística - Academia" />
-        <meta name="keywords" content="academia de música Córdoba, clases de guitarra, clases de piano, clases de canto, clases de violín, folclore, producción musical, home studio, batería, bajo" />
-      </Helmet>
-
       <Header />
       <PrincipalHero />
       <AboutSection /> 
