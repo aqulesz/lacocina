7f9f31a2927331d28824f4cd0f28762ab90ea471
aqulesz <franmoyano123@gmail.com>
2025-06-04 23:28:42 -0300

    seo hecho creo

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,22 @@ import PrincipalHero from './components/PrincipalHero'
 import AboutSection from './components/AboutSection'
 import ClasesSection from './components/ClasesSection'
 import ContactSection from './components/ContactSection'
+import { Helmet } from 'react-helmet';
 
 function App() {
 
   return (
     <>
+      <Helmet>
+        <title>La Cocina Formación y Producción Artística - Academia de musica en Córdoba</title>
+        <meta
+          name="description"
+          content="La Cocina Formación y Producción Artística: Academia de música en Córdoba Capital. Clases de guitarra, piano, canto, folclore, violín, bajo, batería y grabación digital."
+        />
+        <meta name="author" content="La Cocina Formación y Producción Artística - Academia" />
+        <meta name="keywords" content="academia de música Córdoba, clases de guitarra, clases de piano, clases de canto, clases de violín, folclore, producción musical, home studio, batería, bajo" />
+      </Helmet>
+
       <Header />
       <PrincipalHero />
       <AboutSection /> 
--- src/components/CardClase.tsx
+++ src/components/CardClase.tsx
@@ -8,7 +8,7 @@ function CardClase({ title, img, text } : CardClaseProps ) {
     return (
         <div className="bg-custom-red flex flex-col shrink-0" >
             <h4 className="font-hero text-2xl mb-1 md:basis-18 flex-none" >{title}</h4>
-            <img src={img} alt="" className="px-1"/>
+            <img src={img} alt={title} className="px-1"/>
             <p className="font-default font-medium p-2 h-5/10" >{text}</p>
             <a href="https://api.whatsapp.com/send/?phone=543512457260&text&type=phone_number&app_absent=0" className="text-gray-300 underline font-default font-medium block p-2 place-self-end self-end">Inscribite ahora</a>
         </div>
